3fed8cf573a08f252d25ff7584afcfe2496df541
burhanChughtai <mb5905278@gmail.com>
2024-08-26 03:17:16 +0500

    second commit

--- src/components/foodDisplay/FoodDisplay.jsx
+++ src/components/foodDisplay/FoodDisplay.jsx
@@ -8,7 +8,7 @@ const FoodDisplay = ( { categories } ) => {
 
      return (
     <div className='food-display' id='food-display'>
-      <h2>Top dishes near you</h2>
+      <h2>Top dishes near you...</h2>
       <div className="food-display-list">
         {
             food_list.map((item,index)=>{
